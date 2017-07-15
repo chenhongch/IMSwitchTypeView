b3c7f16a81ffe6b54185a3347f029faef26071e4
Git_ChenHong <971063910@qq.com>
2017-07-15 10:45:43 +0800

    Initial Commit

---
+++ IMSwitchTypeView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IMSwitchTypeView
+//
+//  Created by 陈楠 on 17/7/15.
+//  Copyright © 2017年 email:ch_email@126.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
